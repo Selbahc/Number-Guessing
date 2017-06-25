e1eab7deb6c697ad6dbfcc534077cf18d005d435
Selbahc <selbahc@users.noreply.github.comm>
2017-06-25 16:25:50 +0200

    Debugging and adding functionalities

--- devine.js
+++ devine.js
@@ -1,14 +1,29 @@
 var computerNumber = Math.round(Math.random() * 100);
-var userNumber = 0;
+var userNumber = Number(prompt("Guess the computer chosen number (beetween 0 and 100)"));
 
-function compareNumbers(computer, user) {
-  alert(user > computer ? 'Smaller' : user < computer ? 'Greater' : user == computer ? 'Bravo!' : 'Please enter a numerical value');
+var count = 0,
+  indication = '';
+
+function compareNumbers() {
+  while ((count < 9) && (computerNumber != userNumber) && !isNaN(userNumber)) {
+    userNumber > computerNumber ? indication = 'SMALLER' : indication = 'GREATER';
+    userNumber = Number(prompt(` It is ${indication} than ${userNumber}\nTRY AGAIN:`));
+    count++;
+  }
+  continueGame();
 }
 
-for (var i = 0; i < 10; i++) {
-  userNumber = Number(prompt("Guess the computer chosen number (beetween 0 and 100)"));
-  compareNumbers(computerNumber, userNumber);
-  userNumber == computerNumber ? i = 10 : false;
+function continueGame() {
+  if (isNaN(userNumber)) {
+    userNumber = Number(prompt('Please enter a numerical value'));
+    compareNumbers();
+  } else if (count == 9 && userNumber !== computerNumber) {
+    alert(`Game Over !\n(10 attempts max)\n\nIt was ${computerNumber}`);
+  } else if (userNumber == computerNumber) {
+    alert(`BRAVO!\nIt was ${userNumber}!\n\nYou guessed it in ${count} times.`);
+  } else {
+    compareNumbers();
+  }
 }
 
-userNumber != computerNumber ? alert("Game Over (10 attempts max)") : false;
+compareNumbers();
