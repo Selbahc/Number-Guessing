c11340c883b8d9de66ff1d629254c2032eec0a74
Selbahc <selbahc@users.noreply.github.comm>
2017-06-25 13:00:25 +0200

    small refacto

--- devine.js
+++ devine.js
@@ -2,7 +2,7 @@ var computerNumber = Math.round(Math.random() * 100);
 var userNumber = 0;
 
 function compareNumbers(computer, user) {
-  user > computer ? alert("Smaller") : user < computer ? alert("Greater") : user == computer ? alert("Bravo!") : alert("Please enter a numerical value");
+  alert(user > computer ? 'Smaller' : user < computer ? 'Greater' : user == computer ? 'Bravo!' : 'Please enter a numerical value');
 }
 
 for (var i = 0; i < 10; i++) {
